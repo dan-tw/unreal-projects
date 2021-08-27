6e47e7f548e43133bee2c186439b2d2678c8bc56
Daniel Holden <daniel@tensorworks.com.au>
2021-08-27 10:59:31 +1000

    BlockMan initial commit

--- sps/examples/DrivingCxx/Intermediate/ProjectFiles/cmake-csharp-project.cmake
+++
@@ -1,4 +0,0 @@
-set(PROJECT_CSHARP_FILES 
-	"/home/dan/Documents/Unreal Projects/DrivingCxx/Source/DrivingCxx/DrivingCxx.Build.cs"
- )
-
--- sps/examples/DrivingCxx/Intermediate/ProjectFiles/cmake-sources-project.cmake
+++
@@ -1,9 +0,0 @@
-set(PROJECT_SOURCE_FILES 
-	"/home/dan/Documents/Unreal Projects/DrivingCxx/Source/DrivingCxx/DrivingCxx.cpp"
-	"/home/dan/Documents/Unreal Projects/DrivingCxx/Source/DrivingCxx/DrivingCxxGameMode.cpp"
-	"/home/dan/Documents/Unreal Projects/DrivingCxx/Source/DrivingCxx/DrivingCxxHud.cpp"
-	"/home/dan/Documents/Unreal Projects/DrivingCxx/Source/DrivingCxx/DrivingCxxPawn.cpp"
-	"/home/dan/Documents/Unreal Projects/DrivingCxx/Source/DrivingCxx/DrivingCxxWheelFront.cpp"
-	"/home/dan/Documents/Unreal Projects/DrivingCxx/Source/DrivingCxx/DrivingCxxWheelRear.cpp"
- )
-
